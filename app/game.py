ba170259cb5464a68091055b5dc4a7a3ab8204e5
Giovannip11 <giggiopinto@gmail.com>
2025-12-30 15:46:43 -0300

    Some changes in game.py, need more functions to work!!

--- app/game.py
+++ app/game.py
@@ -1,7 +1,16 @@
+from unicodedata import name
 import app
 from app.routes import game
 import random
 
+
+class cards:
+    def __init__(self,name):
+        self.name = name
+
+Empereor=cards("Empereor")
+Slave=cards("Slave")
+Cintizen=cards("Citizen")
 def start_game():
     if app.game is not None:
         app.game.start()
@@ -14,13 +23,30 @@ def _start():
 
 def _shuffle_deck():
     random.shuffle(game.deck)
+def player_chosse_card():
+    if app.game is not None:
+        app.game.player_choose_card()
+    if player_chosse_card()=="Empereor":
+        return Empereor
+    elif player_chosse_card()=="Slave":
+        return Slave    
+    elif player_chosse_card()=="Citizen":
+        return Cintizen 
 
 def _choose_side():
     if game.player_side =="random":
-        game.player_side = random.choice(['X', 'O'])
-        game.computer_side = 'O' if game.player_side == 'X' else 'X'
+        game.player_side = random.choice(['Empereor', 'Slave'])
+        game.computer_side = 'Empereor' if game.player_side == 'Slave' else 'Slave'
     pass
 
+def play_turn():
+    if app.game is not None:
+        app.game.play_turn()
+        app.game.player_choose_card()
+        return player_chosse_card()
+
+
+    
 
 game.start = _start
 game.shuffle_deck = _shuffle_deck
